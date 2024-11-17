f2a606749a3dac180a2b77570d5cebbe9dba2866
Wittawas Nakkasem <vittee@hotmail.com>
2024-11-17 21:54:41 +0700

    chore: add source files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:noble
+
+LABEL org.opencontainers.image.source="https://github.com/vittee/drippybot"
+LABEL org.opencontainers.image.description="DrippyBot"
+LABEL org.opencontainers.image.author="Wittawas Nakkasem <vittee@hotmail.com>"
+LABEL org.opencontainers.image.url="https://github.com/vittee/drippybot/blob/main/README.md"
+LABEL org.opencontainers.image.licenses=MIT
+
+ENV TZ=UTC
+
+RUN apt update -y
+
+RUN apt install fzf cups printer-driver-escpr -y
+
+COPY entrypoint.sh /
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+err() {
+    echo -e >&2 "$@"
+    exit 1
+}
+
+model() {
+    cupsd
+    lpinfo -m | fzf --tac --layout=reverse --with-nth 2.. --bind 'enter:become(echo {1})'
+}
+
+print() {
+    if [ -z ${URI+_} ]; then
+        err No printer URI is specified
+    fi
+
+    if [ -z ${MODEL+_} ]; then
+        err "No printer MODEL is specified\nTry running with \"model\" argument to select a supported model"
+    fi
+
+    cupsd
+
+    # Add the printer
+    lpadmin -p default -v "$URI" -m "$MODEL"
+    # Enable and accept print job
+    cupsaccept default
+    cupsenable default
+    # Set as a default printer
+    lpoptions -d default
+    # Send the print job
+    lp /usr/share/cups/data/testprint
+    # wait
+    sleep ${WAIT:-5}
+}
+
+case "$1" in
+    model)
+        model
+        ;;
+
+    bash)
+        bash
+        ;;
+
+    print)
+        print
+        ;;
+
+    *)
+        sleep infinity
+        ;;
+esac
